c0fb91e23e97c1be7cce0471e12a6b39a4e9701b
邹强 <719160804@qq.com>
2021-04-06 22:53:20 +0800

    2048

---
+++ 2048/Assets/2048/Script/Game2048GameState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Game2048GameState
+{
+    /// <summary>
+    /// 游戏开始
+    /// </summary>
+    Begin,
+    /// <summary>
+    /// 生成数字
+    /// </summary>
+    Build,
+    /// <summary>
+    /// 等待操作
+    /// </summary>
+    Awaits,
+    /// <summary>
+    /// 播放动画
+    /// </summary>
+    Play,
+    /// <summary>
+    /// 游戏结束
+    /// </summary>
+    Over,
+}
